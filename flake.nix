4768d0fcea939b1cf59866de350aae8e7f73bb38
VESSE Léo <leovesse@gmail.com>
2024-11-12 11:31:51 +0100

    initial commit
    adding gitignore
    
    adding backend setup
    
    adding frontend setup
    
    setting up zellij layout
    
    adding packages

---
+++ flake.nix
@@ -0,0 +1,34 @@
+# flake.nix
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/master";
+    };
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      #       ↑ Swap it for your system if needed
+      #       "aarch64-linux" / "x86_64-darwin" / "aarch64-darwin"
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [
+          # backend related
+          cargo
+          rust-analyzer
+          rustfmt
+          # frontend related
+          bun
+          nodePackages.svelte-check
+          nodePackages.svelte-language-server
+          # helpers
+          glow
+        ];
+        shellHook = "";
+      };
+    };
+}
